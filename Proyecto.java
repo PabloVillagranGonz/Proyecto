86cfdf2525d4571f51a0266e31d8fbe5e513e369
Pablo Villagrán <pablovillagrangonz@gmail.com>
2024-03-05 11:56:23 +0100

    Cambios

--- Proyecto.java
+++ Proyecto.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
 public class Proyecto {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int resp=0;
+        boolean salir=false;
+
+        do{
+            try {
+                System.out.println(
+                                "1. Login de administrador\n" +
+                                "2. Login de departamento"
+                );
+                System.out.println("Elige la opción deseada");
+                resp = Integer.parseInt(in.nextLine());
+                salir = true;
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Número no válido, pulse intro para continuar");
+                in.nextLine();
+            }
+        }
+        while(!salir);
+        in.close();
+    }
 }
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Proyecto segundo trimestre
+
+Proyecto realizado por Pablo Villagrán y Cesar Negrín en la asignatura
+de Programación.
+
+### Diagrama de clases:
+
